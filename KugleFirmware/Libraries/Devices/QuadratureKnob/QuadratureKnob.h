9b214aa87579f9c8ef27f87249b47126cc848564
Thomas Jespersen <thomasj@tkjelectronics.dk>
2018-12-18 18:57:39 +0100

    Quadrature knob library implemented and tested

--- KugleFirmware/Libraries/Devices/QuadratureKnob/QuadratureKnob.h
+++ KugleFirmware/Libraries/Devices/QuadratureKnob/QuadratureKnob.h
@@ -20,15 +20,29 @@
 #ifndef DEVICES_QUADRATURE_KNOB_H
 #define DEVICES_QUADRATURE_KNOB_H
 
+#include "stm32h7xx_hal.h"
+#include "IO.h"
+
 class QuadratureKnob
 {
 
 public:
-	QuadratureKnob();	
+	QuadratureKnob(GPIO_TypeDef * GPIOx_A, uint32_t GPIO_Pin_A, GPIO_TypeDef * GPIOx_B, uint32_t GPIO_Pin_B);
 	~QuadratureKnob();
+	int32_t Get();
+
+public:
+	IO * sigA;
+	IO * sigB;
+	
+	int32_t value;
+	uint8_t oldAB;
+
+public:
+	static void InterruptHandler(void * params);
 
 private:
-	
+	static const int8_t EncStates[16];
 };
 	
 	
