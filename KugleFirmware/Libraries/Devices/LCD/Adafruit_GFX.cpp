4e2c2c9bd621afa40ca28ac2ea88922f3283ada4
Thomas Jespersen <thomasj@tkjelectronics.dk>
2019-01-09 20:27:52 +0100

    Updated copyright headers

--- KugleFirmware/Libraries/Devices/LCD/Adafruit_GFX.cpp
+++ KugleFirmware/Libraries/Devices/LCD/Adafruit_GFX.cpp
@@ -34,8 +34,8 @@ POSSIBILITY OF SUCH DAMAGE.
 /* Modified version for use in STM32H7 project by
  * ------------------------------------------
  * Thomas Jespersen, TKJ Electronics
- * Web      :  http://www.tkjelectronics.com
- * e-mail   :  thomasj@tkjelectronics.com
+ * Web      :  http://www.tkjelectronics.dk
+ * e-mail   :  thomasj@tkjelectronics.dk
  * ------------------------------------------
  */
 
